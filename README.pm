f8a47ab283e47572b978c4d026681035d71111bf
Märt Maiste <mart.maiste@gmail.com>
2017-03-19 20:29:18 +0200

    Added initial README.pm

---
+++ README.pm
@@ -0,0 +1,6 @@
+# nextcloud-docker
+
+This nextcloud docker image includes php-fpm, nginx+ssl, letsencrypt. By default it uses sqllite but can be configured to use external Myql or PostgreSQL database.
+
+It is a fork of https://github.com/Wonderfall/dockerfiles/tree/master/nextcloud/11.0 pluss the few additional modifications, mainly SSL and letsencrypt support mainly from https://github.com/ngineered/nginx-php-fpm
+
